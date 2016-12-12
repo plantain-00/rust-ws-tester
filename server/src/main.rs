0d9d6f882fe312fc516ce7fa23f5ec80d32df79a
york yao <yaoao12306@outlook.com>
2016-12-12 21:28:54 +0800

    save

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,15 +2,14 @@ extern crate ws;
 use ws::listen;
 
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App};
 
 fn main() {
     let matches = App::new("ws tester server-side.")
-                      .arg(Arg::with_name("port").takes_value(true))
+                      .arg(Arg::with_name("port").takes_value(true).long("port"))
+                      .arg(Arg::with_name("host").takes_value(true).long("host"))
                       .get_matches();
-
-    let port = matches.value_of("port").unwrap_or(8000);
-    println!("Value for port: {}", port);
-
-    listen("127.0.0.1:3012", |out| move |msg| out.send(msg)).unwrap();
+    let host = matches.value_of("host").unwrap_or("127.0.0.1");
+    let port = matches.value_of("port").unwrap_or("8000").parse::<u16>().unwrap();
+    listen((host, port), |out| move |msg| out.send(msg)).unwrap();
 }
