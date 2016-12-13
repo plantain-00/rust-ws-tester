379f85ef56e695fecfda960d9121d65106421dca
york yao <yaoao12306@outlook.com>
2016-12-14 07:36:45 +0800

    save

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,8 +8,53 @@ fn main() {
     let matches = App::new("ws tester server-side.")
                       .arg(Arg::with_name("port").takes_value(true).long("port"))
                       .arg(Arg::with_name("host").takes_value(true).long("host"))
+                      .arg(Arg::with_name("message-count-per-second")
+                               .takes_value(true)
+                               .long("message-count-per-second"))
+                      .arg(Arg::with_name("message-count-increase")
+                               .takes_value(true)
+                               .long("message-count-increase"))
+                      .arg(Arg::with_name("message-length-increase")
+                               .takes_value(true)
+                               .long("message-length-increase"))
+                      .arg(Arg::with_name("increase-per-second")
+                               .takes_value(true)
+                               .long("increase-per-second"))
                       .get_matches();
-    let host = matches.value_of("host").unwrap_or("127.0.0.1");
+    let host = matches.value_of("host").unwrap_or("localhost");
     let port = matches.value_of("port").unwrap_or("8000").parse::<u16>().unwrap();
-    listen((host, port), |out| move |msg| out.send(msg)).unwrap();
+    let messageLength = matches.value_of("message-length")
+                               .unwrap_or("100")
+                               .parse::<usize>()
+                               .unwrap();
+    let messageCountIncrease = matches.value_of("message-count-increase")
+                                      .unwrap_or("0")
+                                      .parse::<i32>()
+                                      .unwrap();
+    let messageLengthIncrease = matches.value_of("message-length-increase")
+                                       .unwrap_or("0")
+                                       .parse::<i32>()
+                                       .unwrap();
+    let increasePerSecond = matches.value_of("increase-per-second")
+                                   .unwrap_or("0")
+                                   .parse::<i32>()
+                                   .unwrap();
+    let messageCountPerSecond = matches.value_of("message-count-per-second")
+                                       .unwrap_or("1")
+                                       .parse::<i32>()
+                                       .unwrap();
+
+    println!("Listening {}:{}.", host, port);
+    println!("Sending {} message * {} times per second.",
+             messageLength,
+             messageCountPerSecond);
+
+    let message = std::iter::repeat("a").take(messageLength).collect::<String>();
+    println!("{}.", message);
+
+    let mut errorCount = 0;
+    let mut messageCount = 0;
+    let mut messageTotalLength = 0;
+
+    listen((host, port), |ws| move |message| ws.send(message)).unwrap();
 }
